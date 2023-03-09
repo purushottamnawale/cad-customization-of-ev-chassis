95580e654fa9c28b6d32fc2f4f144daf6efade0b
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-09 19:24:06 +0530

    commit

--- test.py
+++ test.py
@@ -1,49 +1,31 @@
-# Given equation
-eqn = lambda B, b, H, h: (B*pow(H,3)/12) - (b*pow(h,3)/12)
+min_lhs = 6127552.083333333
+max_lhs = 7000000
+min_w = 60
+max_w = 200
+min_t = 4
+max_t = 10
 
-# Constants
-MIN_B = 50
-MAX_B = 150
-MIN_b = 45
-MAX_b = 140
-MIN_H = 80
-MAX_H = 200
-MIN_h = 75
-MAX_h = 190
-TARGET = 6127552.083333333
-MAX_ITERATIONS = 1000
+# Start with middle values
+w = (min_w + max_w) / 2
+t = (min_t + max_t) / 2
+lhs = ((w * pow(1.5*w, 3))/12) - (((w-2*t)*pow((1.5*w-2*t), 3))/12)
 
-# Starting values
-B = (MIN_B + MAX_B) // 2
-b = (MIN_b + MAX_b) // 2
-H = (MIN_H + MAX_H) // 2
-h = (MIN_h + MAX_h) // 2
-
-# Iterate through ranges, changing one variable at a time
-for i in range(MAX_ITERATIONS):
-    # Check if current values satisfy equation
-    result = eqn(B, b, H, h)
-    if result > TARGET and result < 10000000:
-        print(f"Found solution after {i+1} iterations:")
-        print(f"B = {B}, b = {b}, H = {H}, h = {h}")
-        break
-
-    # Change variables
-    if result < TARGET:
-        if B < MAX_B:
-            B += 1
-        elif b < MAX_b:
-            b += 1
-        elif H < MAX_H:
-            H += 1
-        elif h < MAX_h:
-            h += 1
+while lhs <= min_lhs or lhs >= max_lhs:
+    if lhs <= min_lhs:
+        # If lhs is too small, increase w and t
+        w += 1
+        t += 0.1
     else:
-        if B > MIN_B:
-            B -= 1
-        elif b > MIN_b:
-            b -= 1
-        elif H > MIN_H:
-            H -= 1
-        elif h > MIN_h:
-            h -= 1
+        # If lhs is too large, decrease w and t
+        w -= 1
+        t -= 0.1
+    lhs = ((w * pow(1.5*w, 3))/12) - (((w-2*t)*pow((1.5*w-2*t), 3))/12)
+
+print(f"w = {w:.2f}, t = {t:.2f}")
+H = round(1.5*w)
+h = round(H-2*t)
+W = round(w)
+w = round(W-2*t)
+
+
+print(W, H, w, h)
--- unknown_finder.py
+++ unknown_finder.py
@@ -1,80 +1,31 @@
-target = 6127552.083333333
-min_target = 6127552.083333333
-max_target = 10000000
+min_lhs = 6127552.083333333
+max_lhs = 7000000
+min_w = 60
+max_w = 200
+min_t = 4
+max_t = 20
 
-# define the ranges for each variable
-W_range = range(50, 151)
-w_range = range(45, 141)
-H_range = range(80, 201)
-h_range = range(75, 191)
+# Start with middle values
+w = (min_w + max_w) / 2
+t = (min_t + max_t) / 2
+lhs = ((w * pow(1.5*w, 3))/12) - (((w-2*t)*pow((1.5*w-2*t), 3))/12)
 
+while lhs <= min_lhs or lhs >= max_lhs:
+    if lhs <= min_lhs:
+        # If lhs is too small, increase w and t
+        w += 1
+        t += 0.1
+    else:
+        # If lhs is too large, decrease w and t
+        w -= 1
+        t -= 0.1
+    lhs = ((w * pow(1.5*w, 3))/12) - (((w-2*t)*pow((1.5*w-2*t), 3))/12)
 
-# initialize the best solution found so far
-best_solution = None
-best_diff = float('inf')
-
-# iterate over all possible combinations of values, starting from the middle values of each range
-for W in range(100, 50, -10):
-    for w in range(93, 45, -10):
-        for H in range(140, 80, -15):
-            for h in range(133, 75, -15):
-                # evaluate the equation for the current values of B, b, H, and h
-                result = (W*pow(H,3)/12) - (w*pow(h,3)/12)
-
-                # check if the result is within the target range and closer to the target than the best solution found so far
-                if min_target < result < max_target and abs(target - result) < best_diff:
-                    best_solution = (W, w, H, h)
-                    best_diff = abs(target - result)
-
-                # if the result is within the target range, we can stop searching
-                if min_target < result < max_target:
-                    break
-
-            if best_solution:
-                break
-
-        if best_solution:
-            break
-
-    if best_solution:
-        break
-
-# iiterate over all possible combinations of values, starting from the middle values of each range
-if not best_solution:
-    for B in range(100, 151, 10):
-        for b in range(93, 141, 10):
-            for H in range(140, 201, 15):
-                for h in range(133, 191, 15):
-                    # evaluate the equation for the current values of B, b, H, and h
-                    result = (B*pow(H,3)/12) - (b*pow(h,3)/12)
-
-                    # check if the result is within the target range and closer to the target than the best solution found so far
-                    if min_target < result < max_target and abs(target - result) < best_diff:
-                        best_solution = (W, w, H, h)
-                        best_diff = abs(target - result)
-
-                    # if the result is within the target range, we can stop searching
-                    if min_target < result < max_target:
-                        break
-
-                if best_solution:
-                    break
-
-            if best_solution:
-                break
-
-        if best_solution:
-            break
-
-# print the best solution found
-if best_solution:
-    print(f"The best solution found is B={best_solution[0]}, b={best_solution[1]}, H={best_solution[2]}, h={best_solution[3]}")
-    print(f"The result is {result}")
-else:
-    print("No solution found within the given range and number of iterations.")
+print(f"w = {w:.2f}, t = {t:.2f}")
+H = round(1.5*w)
+h = round(H-2*t)
+W = round(w)
+w = round(W-2*t)
 
 
-W=best_solution[0]
-w=best_solution[1]
-H=best_solution[2]
-h=best_solution[3]
+print(W, H, w, h)
